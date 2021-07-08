8ae007f82fcc950e31eabfaa4d0df7b8510958fb
Thomas Geoffron <tgeoffron@myges.fr>
2021-07-08 02:06:32 -0700

    Log, register, etc. opérationnels

--- laracartel/vendor/composer/installed.php
+++ laracartel/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '9594dd421617030e81157da262f4ca46203c5716',
+        'reference' => 'c281de0af735cf9e517b7bd739afd05da7ea5acc',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -400,7 +400,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '9594dd421617030e81157da262f4ca46203c5716',
+            'reference' => 'c281de0af735cf9e517b7bd739afd05da7ea5acc',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -419,6 +419,15 @@
             'install_path' => __DIR__ . '/../laravel/tinker',
             'aliases' => array(),
             'reference' => '04ad32c1a3328081097a181875733fa51f402083',
+            'dev_requirement' => false,
+        ),
+        'laravel/ui' => array(
+            'pretty_version' => 'v3.3.0',
+            'version' => '3.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/ui',
+            'aliases' => array(),
+            'reference' => '07d725813350c695c779382cbd6dac0ab8665537',
             'dev_requirement' => false,
         ),
         'league/commonmark' => array(
