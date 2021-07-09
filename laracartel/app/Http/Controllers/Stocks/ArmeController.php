4361fcc2e6cc8182efb876e32d4d30b9bf6156f2
Thomas Geoffron <tgeoffron@myges.fr>
2021-07-09 05:17:27 -0700

    Ajout des controllers

---
+++ laracartel/app/Http/Controllers/Stocks/ArmeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Stocks;
+
+use App\Http\Controllers\Controller;
+use App\Models\Arme;
+use Illuminate\Http\Request;
+
+class ArmeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Arme  $arme
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Arme $arme)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Arme  $arme
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Arme $arme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Arme  $arme
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Arme $arme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Arme  $arme
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Arme $arme)
+    {
+        //
+    }
+}
---
+++ laracartel/app/Http/Controllers/Stocks/EntrepotController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Stocks;
+
+use App\Http\Controllers\Controller;
+use App\Models\Entrepot;
+use Illuminate\Http\Request;
+
+class EntrepotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Entrepot  $entrepot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Entrepot $entrepot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Entrepot  $entrepot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Entrepot $entrepot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Entrepot  $entrepot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Entrepot $entrepot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Entrepot  $entrepot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Entrepot $entrepot)
+    {
+        //
+    }
+}
